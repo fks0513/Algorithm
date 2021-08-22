f8b8e5bc4539873c4983454872da5580ff6710aa
fks0513 <79134045+fks0513@users.noreply.github.com>
2021-08-22 16:47:54 +0900

    Update String_4.java

--- String/String_4.java
+++ String/String_4.java
@@ -6,8 +6,21 @@ import java.util.Scanner;
 public class String_4 {
 	public ArrayList<String> solution(int n, String[] str) {
 		ArrayList<String> answer= new ArrayList<>();
-		for(String x : str) {
-			String tmp = new StringBuilder(x).reverse().toString();
+//		for(String x : str) {
+//			String tmp = new StringBuilder(x).reverse().toString();
+//			answer.add(tmp);
+//		}
+		for(String x:str) {
+			char[] s = x.toCharArray();//단어를 갖는 배열이 char로 전환
+			int lt = 0, rt = x.length()-1;
+			while(lt<rt) {
+				char tmp = s[lt];
+				s[lt]=s[rt];
+				s[rt]=tmp;
+				lt++;
+				rt--;
+			}
+			String tmp = String.valueOf(s); //s라는 문자배열을 String화 시킴(valueOf=static class메서드)
 			answer.add(tmp);
 		}
 		return answer;
